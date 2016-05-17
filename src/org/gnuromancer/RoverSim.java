4e1cdac76dd60c8e932c380e3809e5364f2da9b1
Anuradha Weeraman <aweeraman@gmail.com>
2016-05-17 23:34:58 +0530

    Updated performBehavior method

--- src/org/gnuromancer/RoverSim.java
+++ src/org/gnuromancer/RoverSim.java
@@ -3,6 +3,8 @@ package org.gnuromancer;
 import simbad.gui.Simbad;
 import simbad.sim.Agent;
 import simbad.sim.EnvironmentDescription;
+import simbad.sim.RangeSensorBelt;
+import simbad.sim.RobotFactory;
 
 import javax.vecmath.Vector3d;
 
@@ -10,17 +12,24 @@ public class RoverSim {
 
     public static class Rover extends Agent {
 
+        private RangeSensorBelt sensorBelt;
+
         public Rover(Vector3d position, String name) {
             super(position, name);
 
-            // TODO: Initialize sensors
+            sensorBelt = RobotFactory.addSonarBeltSensor(this, 1);
         }
 
         public void initBehavior() {
         }
 
         public void performBehavior() {
-            // TODO: Move rover
+            setTranslationalVelocity(1.0);
+            double d = sensorBelt.getMeasurement(0);
+            if (d < 0.05) {
+                setRotationalVelocity(Math.PI / 2 * (0.5 - Math.random()));
+            }
+            System.out.println(d);
         }
     }
 
