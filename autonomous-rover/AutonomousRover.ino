358b526918082327b5a38f2a6e940b948da418a5
Anuradha Weeraman <aweeraman@gmail.com>
2016-05-23 18:28:28 +0530

    Added the Arduino project and refactored

---
+++ autonomous-rover/AutonomousRover.ino
@@ -0,0 +1,97 @@
+
+#include <AFMotor.h>
+
+AF_DCMotor front_left(1);
+AF_DCMotor front_right(2);
+
+const int TRIG = 12;
+const int ECHO = 7;
+const int ROVER_SPEED = 75;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode (TRIG, OUTPUT);
+  pinMode (ECHO, INPUT);
+
+  front_left.setSpeed(1);
+  front_left.run(RELEASE);
+
+  front_right.setSpeed(1);
+  front_right.run(RELEASE);
+}
+
+void loop() {
+  move(ping());
+}
+
+int ping() {
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+
+  pinMode(ECHO, INPUT);
+  long duration = pulseIn(ECHO, HIGH);
+  long cm = duration / 29 / 2;
+  
+  Serial.print(cm);
+  Serial.println(" cm");
+  
+  return cm;
+}
+
+void move(long distanceToObstacle) {
+  long rand = random(100);
+  if (distanceToObstacle < 50) {
+      turnRight();
+  } else {
+    moveForward();
+  }
+}
+
+void setRoverSpeed(int speed) {
+  Serial.print("Setting speed ");
+  Serial.println(speed);
+
+  front_left.setSpeed(speed);
+  front_right.setSpeed(speed);
+  
+  delay(5);
+}
+
+void moveForward() {
+  Serial.println("Moving forward");
+  
+  setRoverSpeed(ROVER_SPEED);
+
+  front_left.run(FORWARD);
+  front_right.run(FORWARD);
+}
+
+void stop() {
+  Serial.println("Stopping");
+  setRoverSpeed(0);
+}
+
+void turnRight() {
+  Serial.println("Turning right");
+
+  front_left.setSpeed(ROVER_SPEED);
+  front_left.run(FORWARD);
+
+  front_right.setSpeed(0);
+  front_right.run(FORWARD);
+}
+
+void turnLeft() {
+  Serial.println("Turning left");
+
+  front_left.setSpeed(0);
+  front_left.run(FORWARD);
+
+  front_right.setSpeed(ROVER_SPEED);
+  front_right.run(FORWARD);
+}
+
