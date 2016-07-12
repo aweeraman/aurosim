bc985f6d4052299644306ba3554e803f7a6398ee
Anuradha Weeraman <anuradha@weeraman.com>
2016-07-12 17:03:06 +0530

    Modified to use pin change interrupts

--- AutonomousRover/AutonomousRover.ino
+++ AutonomousRover/AutonomousRover.ino
@@ -1,5 +1,6 @@
 
 #include <AFMotor.h>
+#include <TimerOne.h>
 
 AF_DCMotor front_left(1);
 AF_DCMotor front_right(2);
@@ -7,24 +8,50 @@ AF_DCMotor front_right(2);
 const int TRIG = 12;
 const int ECHO = 7;
 
-const int ROVER_SPEED = 75;
+const int ROVER_SPEED = 100;
 const int ROVER_TURN_SPEED = 100;
+
+const int ENCODER_PIN_RIGHT = 9;
+const int ENCODER_PIN_LEFT = A2;
+
+volatile int right_counter = 0;
+volatile int left_counter = 0;
 
 void setup() {
   Serial.begin(9600);
 
   pinMode (TRIG, OUTPUT);
   pinMode (ECHO, INPUT);
-
+  
   front_left.setSpeed(1);
   front_left.run(RELEASE);
 
   front_right.setSpeed(1);
   front_right.run(RELEASE);
+
+  pciSetup(ENCODER_PIN_RIGHT);
+  pciSetup(ENCODER_PIN_LEFT);
+
+  Timer1.initialize(1000000); // set timer for 1sec
+  Timer1.attachInterrupt( timerIsr ); // enable the timer
 }
 
 void loop() {
   move(ping());
+}
+
+void timerIsr()
+{
+  Timer1.detachInterrupt();  //stop the timer
+  Serial.print("Motor Speed: Right="); 
+  int right_rotation = (right_counter / 2 / 20);
+  int left_rotation = (left_counter / 2 / 20);
+  Serial.print(right_rotation, DEC);  
+  Serial.print(", LEFT="); 
+  Serial.println(left_rotation, DEC);
+  right_counter=0;  //  reset counter to zero
+  left_counter=0;
+  Timer1.attachInterrupt( timerIsr );  //enable the timer
 }
 
 int ping() {
@@ -38,34 +65,34 @@ int ping() {
   long duration = pulseIn(ECHO, HIGH);
   long cm = duration / 29 / 2;
   
-  Serial.print(cm);
-  Serial.println(" cm");
+  //Serial.print(cm);
+  //Serial.println(" cm");
   
   return cm;
 }
 
 void move(long distanceToObstacle) {
   long rand = random(100);
-  if (distanceToObstacle < 50) {
-      turnRight();
-  } else {
-    moveForward();
-  }
+  
+//  if (distanceToObstacle < 50) {
+//    turnRight();
+//  } else {
+//    moveForward();
+//  }
+
+// Move in a straight line for the moment
+  moveForward();
 }
 
 void setRoverSpeed(int speed) {
-  Serial.print("Setting speed ");
-  Serial.println(speed);
 
   front_left.setSpeed(speed);
   front_right.setSpeed(speed);
   
-  delay(5);
+  //delay(5);
 }
 
 void moveForward() {
-  Serial.println("Moving forward");
-  
   setRoverSpeed(ROVER_SPEED);
 
   front_left.run(FORWARD);
@@ -73,13 +100,10 @@ void moveForward() {
 }
 
 void stop() {
-  Serial.println("Stopping");
   setRoverSpeed(0);
 }
 
 void turnRight() {
-  Serial.println("Turning right");
-
   front_left.setSpeed(ROVER_TURN_SPEED);
   front_left.run(FORWARD);
 
@@ -88,12 +112,27 @@ void turnRight() {
 }
 
 void turnLeft() {
-  Serial.println("Turning left");
-
   front_left.setSpeed(0);
   front_left.run(FORWARD);
 
   front_right.setSpeed(ROVER_TURN_SPEED);
   front_right.run(FORWARD);
+}
+
+void pciSetup(byte pin)
+{
+    *digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));  // enable pin
+    PCIFR  |= bit (digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
+    PCICR  |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
+}
+
+ISR (PCINT0_vect) // handle pin change interrupt for D0 to D7 here
+{
+    right_counter++;
+}  
+
+ISR (PCINT1_vect) // handle pin change interrupt for D8 to D13 here
+{    
+    left_counter++;
 }
 
